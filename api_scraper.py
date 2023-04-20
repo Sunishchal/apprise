12e314106e1a353e5c367204afd2e745218157f3
Sunishchal Dev <sunishchaldev@gmail.com>
2023-04-20 00:48:26 -0500

    sendgrid setup WIP

--- api_scraper.py
+++ api_scraper.py
@@ -12,7 +12,7 @@ from pyairtable import Table
 import pandas as pd
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail, Substitution
+from sendgrid.helpers.mail import Mail
 
 
 def TableDF(table):
@@ -35,29 +35,7 @@ def TableDF(table):
     else:
         return None
 
-# Use the GPT tokenizer to figure out the word count of the input
-def num_tokens_from_string(string: str, encoding_name: str) -> int:
-    encoding = tiktoken.encoding_for_model(encoding_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
-
-@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
-def gpt_summary(gpt_model, summary_size, abstracts):
-    summary_limit = 8000 if summary_size > 5333 else int(summary_size * 1.5)
-    return openai.ChatCompletion.create(
-                  model=gpt_model,
-                  messages=[
-                    {"role": "system", "content": f"You are an expert on US Government who will read abstracts from the Federal Register and generate easy to understand summaries using less than {summary_size} tokens. Please use proper grammar and punctuation, write in pragraph form, and avoid run-on sentences."},
-                    {"role": "user", "content": abstracts}
-                  ],
-                  max_tokens=summary_limit,
-                  temperature=1,
-                  presence_penalty=1,
-                  frequency_penalty=1
-                )
-
-
-def run():
+def run(i):
     # Retrieve API Keys & Gmail app password from env variables
     OPENAI_KEY = os.getenv('OPENAI_KEY')
     openai.api_key = OPENAI_KEY
@@ -81,11 +59,12 @@ def run():
         date = datetime.date.today()
         
     year = date.strftime("%Y")
-    month = date.strftime("%m")
-    day = date.strftime("%d")
+    month = '03' # date.strftime("%m")
+    day = str(int(date.strftime("%d")) + i)
+    print(f'{year}-{month}-{day}')
 
     # Define the API endpoint to retrieve today's Federal Register documents
-    url = f"https://www.federalregister.gov/api/v1/issues/{year}-{'03'}-{'28'}.json"
+    url = f"https://www.federalregister.gov/api/v1/issues/{year}-{month}-{day}.json"
 
     # Make the API request
     response = requests.get(url)
@@ -126,61 +105,13 @@ def run():
 
                 # Append the abstract to a text file, skipping the documents with no abstract or PRA related text
                 if doc_abstract is not None and 'Paperwork Reduction Act' not in doc_abstract:
-                    abstracts += doc_abstract + '<br><br>'
-                    formatted_abstracts += f"{title}: <br>{doc_abstract} <br>Full document: {pdf_url}<br><br>"
+                    abstracts += doc_abstract + '\n\n'
+                    formatted_abstracts += f"{title}: \n{doc_abstract} \n"#Full document: {pdf_url}\n\n"
 
-            # Make a summary that's about 30% the size of the abstracts
-            token_count = num_tokens_from_string(abstracts, gpt_model)
-            summary_size = round(token_count * 0.3)
-
-            # Check if input text is within the GPT-3 token limit 
-            if token_count + summary_size <= 8000 and len(abstracts) > 0:
-
-                # Call the Chat GPT-3.5 API asking for a summary
-                response = gpt_summary(gpt_model, summary_size, abstracts)
-
-                # Extract the summary from the API response
-                summary = response["choices"][0]["message"]["content"]
-
-                result += f"{sub_interest}:<br>" + summary + "<br><br>"
-
-            else:
-                result += f"There are no {sub_interest} documents published today.<br><br>"
-
-        # update to your dynamic template id from the UI
-        TEMPLATE_ID = 'd-9fd5ba827e824a5ea38c2d5607883370'
-        subject_line = 'Apprise Daily Summary ' + str(date)
-        
-        message = Mail(
-            from_email=from_email,
-            to_emails=to_email,
-            subject=subject_line
-        )
-
-        # set the dynamic template content of the Mail object
-        message.template_id = TEMPLATE_ID
-        message.dynamic_template_data = {
-            'summary': [Substitution('1', result)],
-            'full_text': [subject_line]
-        }
-
-        print(result)
-        print(formatted_abstracts)
-
-        try:
-            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-            response = sg.send(message)
-            code, body, headers = response.status_code, response.body, response.headers
-            print(f"Response code: {code}")
-            print(f"Response headers: {headers}")
-            print(f"Response body: {body}")
-            print("Dynamic Messages Sent!")
-        except Exception as e:
-            print("Error: {0}".format(e))
-
-        to_emails.append(to_email)
-
-    print('Sent emails to ', to_emails)
+    # write formatted_abstracts to a text file
+    with open('abstracts.txt', 'a') as f:
+        f.write(formatted_abstracts)
 
 if __name__ == '__main__':
-    run()
+    for i in range(31):
+        run(i)
